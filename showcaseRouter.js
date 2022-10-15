9617be888fc4dd8e2860b1b4115a71a45056ecb3
tffaruk <97652722+tffaruk@users.noreply.github.com>
2022-10-15 15:15:21 +0600

    add delete

--- showcaseRouter.js
+++ showcaseRouter.js
@@ -138,6 +138,22 @@ showcaseHandler.patch("/trash/:id", (req, res) => {
     }
   ).clone();
 });
+//
+// delete download field
+showcaseHandler.get("/delete/:id", async (req, res) => {
+  await dbConnect();
+  console.log(req.params.name);
+  Showcase.deleteOne({ _id: req.params.id }, function (err, data) {
+    if (!err) {
+      console.log(data);
+
+      console.log("member successfully deleted");
+    } else {
+      console.log(err);
+    }
+  });
+  res.redirect("/");
+});
 
 module.exports = showcaseHandler;
 
